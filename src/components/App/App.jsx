218e6f56c06c664f83a3dc669a823477235e1184
Jason Henrickson <jason.henrickson15@gmail.com>
2022-02-07 16:32:09 -0600

    gallery items displaying

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
+import GalleryList from '../GalleryList/GalleryList';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Gallery of My Life</h1>
+      </header>
+      <GalleryList />
+    </div>
+  );
 }
 
 export default App;
